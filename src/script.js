820048a823f49982358658d15e8654cee7c5a762
Fadhil <muhammadfadhilsalasiah@gmail.com>
2025-03-22 15:20:20 +0800

    update 3

--- src/script.js
+++ src/script.js
@@ -3,27 +3,37 @@ const buttons = document.querySelectorAll('.btn');
 const clear = document.getElementById('clear');
 const hasil = document.getElementById('Hasil');
 const hapus = document.getElementById('hapus');
+const operators = document.querySelectorAll('.operator');
 
-
-let currentInput = ''
-
+let currentInput = '';
+let operatorPilihan = ';'
+let newvalue = '';
+let rawValue = '';
 buttons.forEach((button)=>{
     button.addEventListener('click',function(){
         const value = button.textContent;
 
         if(value !== 'C' && value !== '='){
-            let rawValue = currentInput.replace(/,/g,'');
+          
+               rawValue += value;
 
-            rawValue += value
-     
-            currentInput = Number(rawValue).toLocaleString("en-US");
-            output.value = currentInput
+            
+               output.value += rawValue
+              console.log(currentInput);
         }
       
     })
 })
 
-
+operators.forEach((operator)=>{
+    operator.addEventListener('click',function(e){
+      operatorPilihan = operator.textContent;
+      console.log(operatorPilihan);
+      output.value += operatorPilihan;
+      rawValue = '';
+     
+    })
+})
 
 clear.addEventListener('click',function(){
     currentInput = '';
@@ -41,7 +51,7 @@ output.value = currentInput;
 
 hasil.addEventListener('click',function(){
     try{
-        currentInput =  eval(currentInput);
+        currentInput =  eval(output.value);
         output.value = currentInput
     }catch(err){
         output.value = 'Error'
